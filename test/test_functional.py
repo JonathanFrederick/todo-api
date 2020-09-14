61b3050e8d63872a32cef9d9065bba3fdc8f8f08
Jonathan Frederick <jrfreder@gmail.com>
2020-09-14 19:44:14 -0400

    testing for a bit more content and passing

--- test/test_functional.py
+++ test/test_functional.py
@@ -13,6 +13,12 @@ class LandingPageTestClass(TestCase):
         """User notices a somewhat descriptive title"""
         assert 'To-Do' in self.browser.title
 
+    def test_description(self):
+        """User reads a description of the site and sees a link for the project repo"""
+        body = self.browser.find_element_by_tag_name("body").text
+        assert 'This is the landing page for a to-do list api' in body
+        assert 'https://github.com/JonathanFrederick/todo-api/' in body
+
     @classmethod
     def tearDownClass(cls):
         cls.browser.close()
