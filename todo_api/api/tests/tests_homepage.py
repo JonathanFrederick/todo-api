54c14be22f07d0cb1bd9f4caee72221f19255706
Jonathan Frederick <jrfreder@gmail.com>
2020-09-14 19:42:24 -0400

    providing title, testing view and url path

---
+++ todo_api/api/tests/tests_homepage.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.urls import resolve
+from api.views import home_page
+
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_homepage_returns_correct_html(self):
+        response = self.client.get('/')
+
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do API</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'api/index.html')
--- todo_api/api/views.py
+++ todo_api/api/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
+def home_page(request):
+    return render(request, "api/index.html")
